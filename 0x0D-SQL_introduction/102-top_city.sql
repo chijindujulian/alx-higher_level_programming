25713deb7f8bdefc74a38e8142bbbe083daaefc2
chijindu julian <chijindujulian@gmail.com>
2024-05-23 04:34:59 -0700

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,7 @@
 -- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
-SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
-SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
+
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures` WHERE `month` = 7 and `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp`
+DESC LIMIT 3;
