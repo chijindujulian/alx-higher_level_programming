31d884dc66afe1456e7b5e712f85c26ed0544ace
chijindu julian <chijindujulian@gmail.com>
2024-05-23 04:42:02 -0700

    103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
 
-SELECT `city`, MAX(`value`) AS `max_temp`
+SELECT `state`, MAX(`value`) AS `max_temp`
 GROUP BY `state`
 ORDER BY `state`
 DESC LIMIT 3;
